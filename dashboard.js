a7883da93a13cd5b49e93051403884a4419b84a1
ruinzyu <dharuaulia1@gmail.com>
2023-06-07 13:38:20 +0700

    first commit

---
+++ dashboard.js
@@ -0,0 +1,55 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(getSpreadsheetData);
+
+function getSpreadsheetData() {
+  var spreadsheetId = "1uGopZFDGGgWaSM70kXzdqGVbd6_tmFcaTfYJpwRFONs";
+  var range = "qty_by_region!A1:E3";
+
+  var query = new google.visualization.Query(
+    "https://docs.google.com/spreadsheets/d/" +
+      spreadsheetId +
+      "/gviz/tq?gid=0&range=" +
+      range
+  );
+  query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    console.error("Error: " + response.getMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+  drawCharts(data);
+}
+
+function drawCharts(data) {
+  var options = {
+    title: "Grafik Ketersediaan Kategori Makanan di Berbagai Wilayah",
+    width: 450,
+    height: 300,
+  };
+
+  var chart1 = new google.visualization.ColumnChart(
+    document.getElementById("chart1")
+  );
+  chart1.draw(data, options);
+
+  var chart2 = new google.visualization.PieChart(
+    document.getElementById("chart2")
+  );
+  chart2.draw(data, options);
+
+  var chart3 = new google.visualization.AreaChart(
+    document.getElementById("chart3")
+  );
+  chart3.draw(data, options);
+
+  var chart4 = new google.visualization.ScatterChart(
+    document.getElementById("chart4")
+  );
+  chart4.draw(data, options);
+
+  // Tambahkan jenis grafik lain dan elemen div untuk grafik lainnya
+}
